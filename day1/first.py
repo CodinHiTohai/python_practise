b2b791384b2cd4780e3e2f9b880ff1d1d4618ec4
GovindKumar <govindkumar52936@gmail.com>
2026-01-18 13:03:04 +0530

    practise

---
+++ day1/first.py
@@ -0,0 +1,62 @@
+# a=int(input("enter the first number"))
+# b=int(input("enter the second number"))
+# c=int(input("enter the second number"))
+# avg=(a+b+c)/3
+# print(avg)
+#positional arguments
+# def avg(a,b,c):
+#    return (a+b+c)/3
+
+# print(avg(3,6,8))
+
+#default arguments
+# def greet(name="govind"):
+#     print( f"hello {name}")
+# greet()
+# def students(name,age):
+#     print(f"name is{name} and age is {age}")
+# students(name="govind",age=22)
+
+# add=lambda x,y:x+y
+# print(add(4,3))
+# avg=lambda x,y,z:(x+y+z)/3
+# print(avg(1,2,3))
+
+# n=int(input("enter the numbe you want factorial"))
+# fact=1;
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(fact)/
+
+# def factorial(n):
+#      if n==0:
+#           return 1
+#      return n*factorial(n-1)
+# print(factorial(5))
+
+# def sum_of_nnumber(n):
+#      if n==1:
+#           return 1
+#      return n+sum_of_nnumber(n-1)
+# print(sum_of_nnumber(10))
+# number=[1,2,3,4,5,6]
+# print(number[0])
+# print(number[-1])
+# number.append(9)
+# print(number)
+# number.insert(2,88)
+# print(number)
+# number.remove(5)
+# print(number)
+# number.pop()
+# print(number)
+# number.remove
+# print(number)
+# number.sort
+# print(number
+square=[i*i for i in range(1,6)]
+print(square)
+
+even=[i for i in range(1,11)if i%2==0]
+print(even)
+
